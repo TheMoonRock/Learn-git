a2e6d25c1b84fadf6a00d72e4910620b8b7b4587
TheMoonRock <mr.artyomgalimov@gmail.com>
2020-06-04 16:50:18 +0300

    fahr-celsius

--- main.c
+++ main.c
@@ -1,32 +1,18 @@
 #include <stdio.h>
- 
-main() {
-   float x,y;
-   char sign='+';
-   while (sign != '0') {
-      printf("Знак: ");
-      scanf("%c%*c", &sign);
-      if (sign == '0') { break; }
-      if (sign == '+' || sign == '-' || sign == '*' || sign == '/') {
-         printf("x=");
-         scanf("%f%*c", &x);
-         printf("y=");
-         scanf("%f%*c", &y);
-         switch (sign) {
-            case '+':
-               printf("%.2f\n", x+y);
-               break;
-            case '-':
-               printf("%.2f\n", x-y);
-               break;
-            case '*':
-               printf("%.2f\n", x*y);
-               break;
-            case '/':
-               if (y != 0) printf("%.2f\n", x/y);
-               else printf("Деление на ноль!\n");
-         }
-      }
-      else printf("Неверно указан знак операции\n");
+
+int main ()
+{
+   int fahr, celsius;
+   int lower, upper, step;
+
+   lower = 0;
+   upper = 300;
+   step = 20;
+
+   fahr = lower;
+   while (fahr <= upper) {
+      celsius = 5 * (fahr-32) / 9;
+      printf("%d\t%d\n", fahr, celsius);
+      fahr = fahr + step;
    }
 }
